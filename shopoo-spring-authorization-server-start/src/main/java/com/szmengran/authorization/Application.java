04e0e6dd56b4196aca21a69e47509f4c42ccea4b
李茂源 <android_li@sina.cn>
2023-05-11 20:53:14 +0800

    基础架构升级

--- shopoo-spring-authorization-server-start/src/main/java/com/szmengran/authorization/Application.java
+++ shopoo-spring-authorization-server-start/src/main/java/com/szmengran/authorization/Application.java
@@ -3,6 +3,7 @@ package com.szmengran.authorization;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author Frank Zhang
  */
 @EnableDubbo
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.szmengran.authorization.infrastructure.wechat.client"})
 @SpringBootApplication(scanBasePackages = {"com.szmengran.authorization", "com.alibaba.cola"})
 public class Application {
@@ -18,6 +20,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
 }
 
 
--- shopoo-spring-authorization-server-start/src/test/java/com/szmengran/authorization/TestApplication.java
+++ shopoo-spring-authorization-server-start/src/test/java/com/szmengran/authorization/TestApplication.java
@@ -1,11 +1,41 @@
 package com.szmengran.authorization;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.Event;
+import com.alibaba.nacos.api.naming.listener.EventListener;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
+import org.junit.Test;
+import java.util.Properties;
 
 public class TestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Test
+    public static void main(String[] args) throws NacosException {
+        Properties properties = new Properties();
+        properties.setProperty("serverAddr", "www.szmengran.com:8848?username=nacos&password=@");
+        properties.setProperty("namespace", "szmengran-dev");
+        properties.put("username","");
+        properties.put("password","@");
+        NamingService naming = NamingFactory.createNamingService(properties);
+    
+        naming.registerInstance("shopoo-wechat-start", "11.11.11.11", 8888, "TEST1");
+    
+        naming.registerInstance("shopoo-wechat-start", "2.2.2.2", 9999, "DEFAULT");
+    
+        System.out.println(naming.getAllInstances("shopoo-wechat-start"));
+    
+        naming.deregisterInstance("shopoo-wechat-start", "2.2.2.2", 9999, "DEFAULT");
+    
+        System.out.println(naming.getAllInstances("shopoo-wechat-start"));
+    
+        naming.subscribe("shopoo-wechat-start", new EventListener() {
+            @Override
+            public void onEvent(Event event) {
+                System.out.println(((NamingEvent)event).getServiceName());
+                System.out.println(((NamingEvent)event).getInstances());
+            }
+        });
     }
 }
